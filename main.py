d2fecf41eee23674b565da66f29cab28bd5f9b72
Ali <abdulrehman76261@gmail.com>
2022-04-22 06:32:35 -0700

    strong password generator

---
+++ main.py
@@ -0,0 +1,28 @@
+from cgi import print_arguments
+import string
+import random
+
+if __name__ == "__main__":
+    s1 = string.ascii_lowercase
+    # print(s1)
+
+    s2 = string.ascii_uppercase
+    # print(s2)
+
+    s3 = string.digits
+    # print(s3)
+
+    s4 = string.punctuation
+    # print(s4)
+
+    
+    plen = int(input("Enter password length \n"))
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    # print(s)
+    random.shuffle(s)
+    # print(s)
+    print("".join(s[0:plen]))
